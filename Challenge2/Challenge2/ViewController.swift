cf90d0602dc02039c44a2bcf9988cc1fa5d034f8
JuanMLondon <jmlondonr@gmail.com>
2022-11-21 13:23:56 -0500

    Subiendo app funcional

--- Challenge2/Challenge2/ViewController.swift
+++ Challenge2/Challenge2/ViewController.swift
@@ -9,11 +9,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var diceImageView: UIImageView!
+    
+    let diceArray = [UIImage (named: "dice_1"), UIImage (named: "dice_2"), UIImage (named: "dice_3"), UIImage (named: "dice_4"), UIImage (named: "dice_5"), UIImage (named: "dice_6")]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        diceImageView.alpha = 0.3
+        diceImageView.image = diceArray[Int.random(in: 0...5)]
     }
 
-
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView.alpha = 1
+        diceImageView.image = diceArray[Int.random(in: 0...5)]
+    }
 }
 
